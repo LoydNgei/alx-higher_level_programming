10aec496dc23aa8e746dd3214f01d938920849ae
loyd ngei <loydngei@gmail.com>
2022-12-05 12:42:32 +0300

    max element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    largest = int(my_list[0])
+    largest = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > largest:
             largest = my_list[i]
 
-            return(largest)
+            return (largest)
