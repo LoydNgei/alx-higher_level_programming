900dc6ebd5c0c9fc923293448e8d822a261057ad
loyd ngei <loydngei@gmail.com>
2022-12-07 13:39:02 +0300

    print the max_int in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
     largest = my_list[0]
     for i in my_list:
         if i > largest:
-            largest_int = i
+            largest = i
 
-            return (largest_int)
+            return (largest)
