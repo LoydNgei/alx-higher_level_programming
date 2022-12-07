7802221274ec7d21fae650c296097e24fd5cd5f7
loyd ngei <loydngei@gmail.com>
2022-12-07 13:25:46 +0300

    length of string and first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
-^#!/usr/bin/python3
+#!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence == "":
+    if len(sentence) == 0:
         character = None
     else:
         character = sentence[0]
