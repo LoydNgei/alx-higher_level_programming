050ced5c880e399aa720c8df36187a31900be076
loyd ngei <loydngei@gmail.com>
2022-12-07 13:20:59 +0300

    length of string and first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        character = None
+    else:
+        character = sentence[0]
+        return(len(sentence), character)
