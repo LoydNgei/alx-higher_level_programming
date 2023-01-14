da1bff646e56a11cbbdf5103ad9fc1356ffd467b
loyd ngei <loydngei@gmail.com>
2023-01-14 11:25:12 +0300

    Class rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
             TypeError: if value is not an integer
             ValueError: If value is less or equal to 0
             """
-        if not isinstance(value, int):
+        if (type(value) != int):
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
