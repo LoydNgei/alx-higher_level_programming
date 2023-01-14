f7c2820c9a303595a91ec3648481130b0ed346a6
loyd ngei <loydngei@gmail.com>
2023-01-14 09:21:37 +0300

    pycodestyle Error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,14 +5,14 @@
 def is_same_class(obj, a_class):
     """Check if an object is exactly an instance of a given class
    Args:
-   	obj: The object to check
-	a_class: The class to match the type of obj to
+     obj: The object to check
+     a_class: The class to match the type of obj to
    Returns:
-   	True - if obj is exactly an instance of obj to
-	False - Otherwise
+       True - if obj is exactly an instance of obj to
+       False - Otherwise
     """
 
     if type(obj) == a_class:
-       return True
+        return True
     else:
-       return False
+        return False
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-   """Returns True if obj is an instance of, or obj is a class instance
+    """Returns True if obj is an instance of, or obj is a class instance
    that is inherited from specialized class else return False"""
-   if isinstance(obj, a_class):
-       return True
-   return False
+    if isinstance(obj, a_class):
+        return True
+    return False
