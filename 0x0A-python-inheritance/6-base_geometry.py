460be24a6a2d3f2317d55cf3e5cfb1db5fcc0783
loyd ngei <loydngei@gmail.com>
2023-01-14 10:35:58 +0300

    Improved class Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class with a public instance method"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
