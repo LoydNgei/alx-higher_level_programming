81adc650dd3ea9994f116378b8ce803047f61cc4
loyd ngei <loydngei@gmail.com>
2023-01-17 12:39:17 +0300

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,13 +17,13 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the student
-        If attrs is a list of strings, represents only those attributes 
+        If attrs is a list of strings, represents only those attributes
         included in the list
 
         Args:
             attrs (list): The attributes to represent.
         """
-        if (type(attrs) == list and 
+        if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
