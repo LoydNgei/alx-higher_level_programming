0da63574bacc7dafbcda962090e603d9002893fb
loyd ngei <loydngei@gmail.com>
2023-01-17 12:03:30 +0300

    class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a python class-to-JSON function"""
 
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
