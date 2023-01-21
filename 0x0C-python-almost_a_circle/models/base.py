3a8516e0245e23f768a6762819d46b3d80f5d442
loyd ngei <loydngei@gmail.com>
2023-01-21 14:55:33 +0300

    models/Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,13 +2,20 @@
 """Define a class Base"""
 
 
-__nb_object = 0
+class Base:
+    """Represents the base model
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases
+        """
+        __nb_object = 0
 
-
-def __init__(self, id=None):
-    """Initialize the class constructor"""
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_object += 1
-        self.id = Base.__nb_objects
+        def __init__(self, id=None):
+            """Initialize the class constructor
+            Args:
+                id (int): The identity of the new Base
+            """
+            if id is not None:
+                self.id = id
+            else:
+                Base.__nb_object += 1
+                self.id = Base.__nb_objects
