75fbd1e550710f5990a766402d0f4dff9c43efe2
loyd ngei <loydngei@gmail.com>
2023-02-16 15:41:48 +0300

    SQL is fun

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- A script that deletes from a table
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- A script that lists all records in a table second_table
+SELECT score, name
+FROM second_table 
+WHERE name != ""
+ORDER BY score DESC;
