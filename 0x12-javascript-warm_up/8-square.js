e04ab04c63df3d5e23202c0a8f90d79f2408128d
loyd ngei <loydngei@gmail.com>
2023-03-15 01:58:22 +0300

    Square in X's

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (!size) {
+  console.log('Missing size');
+}
+for (let i = 0; i < size; i++) {
+  let row = '';
+
+  for (let j = 0; j < size; j++) {
+    row += 'X';
+  }
+  console.log(row);
+}
