2a64bd5914baf5b1a95da9785dbae9dd7b26237e
loyd ngei <loydngei@gmail.com>
2023-03-15 01:24:47 +0300

    Array of str and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const mystrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const mystrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < mystrings.length; i++) {
   console.log(mystrings[i]);
