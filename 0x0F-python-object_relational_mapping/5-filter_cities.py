99e750a7b09304ff484bda5bc2670ac0a4d6493a
loyd ngei <loydngei@gmail.com>
2023-03-25 16:35:11 +0300

    fetch first

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     mycursor.execute("SELECT cities.id, cities.name FROM cities \
             JOIN states ON cities.state_id WHERE \
             states.name LIKE BINARY %(state_name)s \
-            ORDER BY cities.id ASC", {'state_name': sys.argv[4], })
+            ORDER BY cities.id ASC", sys.argv[4])
 
     rows_selected = mycursor.fetchall()
 
