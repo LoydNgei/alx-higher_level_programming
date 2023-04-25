bebc35b1c46ec555fdca6553dcfa9840d9a40ec4
loyd ngei <loydngei@gmail.com>
2023-04-25 12:56:03 -0700

    Count

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,14 +6,14 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err) console.error(err);
-  const task = JSON.parse(body);
+  const tasks = JSON.parse(body);
   const dict = {};
-  for (let i = 0; i < task.length; i++) {
-    if (!dict[task[i].userId]) {
-      dict[task[i].userId] = 0;
+  for (let i = 0; i < tasks.length; i++) {
+    if (!dict[tasks[i].userId]) {
+      dict[tasks[i].userId] = 0;
     }
-    if (task[i].completed === true) {
-      dict[task[i].userId] += 1;
+    if (tasks[i].completed === true) {
+      dict[tasks[i].userId] += 1;
     }
   }
   console.log(dict);
